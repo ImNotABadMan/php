5043e35531be84664560f30e1dace7dc443a14c6
binz <bin_zheng@limei-trading.com>
2018-09-17 10:41:44 +0800

    test

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<?php
 /**
  * @Author: anchen
  * @Date:   2017-12-29 19:36:28
@@ -6,4 +6,5 @@
  * @Last Modified time: 2017-12-29 19:55:54
  */
 echo "one code master";
-sadsad
+
+var_dump(123);
