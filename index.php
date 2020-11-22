a12f5f9427791625e1f2bdfcb4e038b54151fc06
binz <876507628@qq.com>
2020-11-22 21:58:24 +0800

    jenkins pipeline test

--- index.php
+++ index.php
@@ -9,3 +9,24 @@ echo "one code master";
 
 var_dump(123);
 var_dump(132);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+    <?php
+        var_dump("直接些php脚本形式");
+    ?>
+</h1>
+
+</body>
+</html>
